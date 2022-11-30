d165e4146a1487e36ccad13b44924512d1b608fe
Ayush <abaran803@gmail.com>
2022-12-01 02:07:46 +0530

    Moving DB name to .env

--- src/Config/DatabaseConfig.js
+++ src/Config/DatabaseConfig.js
@@ -1,6 +1,6 @@
 // Configuring the Database here
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.MONGO_URI}/ff`)
+mongoose.connect(process.env.MONGO_URI)
 .then(res => console.log("Connected to Database"))
 .catch(err => console.log("Error:", err.message));
